a440205b3be868c7cd2f6c4c2350afcd49a5a7e7
Felipe Panizio <fpanizio10@gmail.com>
2025-12-10 11:36:43 -0300

    Update client fetch URL and add client script to package.json

--- client.mjs
+++ client.mjs
@@ -1,6 +1,6 @@
 const base = 'http://localhost:3000';
 
 setTimeout(async () => {
-  const reponse = await fetch(base + '/');
+  const reponse = await fetch(base + '/curso/javascript');
   console.log(reponse.ok, reponse.status);
 }, 200);
