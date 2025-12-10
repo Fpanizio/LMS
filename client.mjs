1f04bfde71b5ad1bfa6cd3175e7e37cc9814d2de
Felipe Panizio <fpanizio10@gmail.com>
2025-12-10 11:03:25 -0300

    Organize the structure

--- client.mjs
+++ client.mjs
@@ -1,16 +1,6 @@
-import { HOST, PORT } from "./config.mjs";
+const base = 'http://localhost:3000';
 
-const response = await fetch(`http://${HOST}:${PORT}/product?color=red`, {
-  method: "POST",
-  // headers: {
-  //   "Content-Type": "application/json",
-  // },
-  // body: JSON.stringify({
-  //   username: "user123",
-  //   password: "securepassword",
-  // }),
-});
-
-console.log(response);
-const body = await response.text();
-console.log(body);
+setTimeout(async () => {
+  const reponse = await fetch(base + '/');
+  console.log(reponse.ok, reponse.status);
+}, 200);
