e58e7d751bda584216fe3ac4d6742cb3ba9e1df4
Felipe Panizio <fpanizio10@gmail.com>
2025-12-11 09:42:14 -0300

    post

--- api/lms/index.ts
+++ api/lms/index.ts
@@ -1,8 +1,45 @@
 import { Api } from "../../core/utils/abstract.ts";
 import { lmsTables } from "./tables.ts";
+import { RouteError } from "../../core/utils/route-error.ts";
 
 export class LmsApi extends Api {
+
+    handlers = {
+        postCourses: (req, res) => {
+            const { slug, title, description, lessons, hours } = req.body;
+            const writeResult = this.db.query(/* sql */ `
+                INSERT OR IGNORE INTO courses 
+                ("slug", "title", "description", "lessons", "hours") VALUES (?, ?, ?, ?, ?);
+            `).run(slug, title, description, lessons, hours);
+            if (writeResult.changes === 0) {
+                throw new RouteError('Course already exists', 400);
+            }
+            res.status(201).json({ id: writeResult.lastInsertRowid, title: "Created course" });
+        },
+        
+        postLessons: (req, res) => {
+            const { courseSlug, slug, title, seconds, video, description, order, free } = req.body;
+
+            const writeResult = this.db.query(/* sql */ `
+                INSERT OR IGNORE INTO lessons 
+                ("course_id", "slug", "title", "seconds", "video", "description", "order", "free" )
+                 VALUES ((SELECT "id" FROM "courses" WHERE "slug" = ?), ?, ?, ?, ?, ?, ?, ?);
+            `).run(courseSlug, slug, title, seconds, video, description, order, free);
+
+            if (writeResult.changes === 0) {
+                throw new RouteError('Lesson already exists', 400);
+            }
+            res.status(201).json({ id: writeResult.lastInsertRowid, title: "Created lesson" });
+        }
+    } satisfies Api['handlers']
+
     table(): void {
         this.db.exec(lmsTables);
+    }
+
+    routes(): void {
+
+        this.router.post('/lms/courses', this.handlers.postCourses);
+        this.router.post('/lms/lessons', this.handlers.postLessons);
     }
 }
--- client.mjs
+++ client.mjs
@@ -2,12 +2,47 @@ console.clear()
 const base = 'http://localhost:3000';
 
 const functions = {
-  async getProducts() {
-    const response = await fetch(base + '/products/notebook');
+  async postCourses() {
+    const response = await fetch(base + '/lms/courses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        slug: 'Html-e-css',
+        title: 'HTML e CSS',
+        description: 'HTML and CSS course for beginners',
+        lessons: 40,
+        hours: 10,
+      }),
+    });
     const body = await response.json();
     console.table(body);
     console.log("------------END------------");
-  }
+  },
+
+  async postLessons() {
+    const response = await fetch(base + '/lms/lessons', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        courseSlug: 'Html-e-css',
+        slug: 'basic-tags',
+        title: 'Basic Tags',
+        seconds: 200,
+        // video: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        video: '/html/basic-tags.mp4',
+        description: 'Basic tags of HTML',
+        order: 1,
+        free: 1,
+      }),
+    });
+    const body = await response.json();
+    console.table(body);
+    console.log("------------END------------");
+  },
 }
 
 functions[process.argv[2]]();
